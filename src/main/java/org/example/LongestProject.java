5200d969ce5f7293555b29caecd87922a1d6c095
VVVladyslav <vladoszhar@gmail.com>
2023-12-23 23:06:15 +0200

    g

---
+++ src/main/java/org/example/LongestProject.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class LongestProject {
+    private int id;
+    private int idClient;
+    private int StartData;
+    private int FinishData;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public int getStartData() {
+        return StartData;
+    }
+
+    public void setStartData(int startData) {
+        StartData = startData;
+    }
+
+    public int getFinishData() {
+        return FinishData;
+    }
+
+    public void setFinishData(int finishData) {
+        FinishData = finishData;
+    }
+}
