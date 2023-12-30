133514a54ac8e1e44d9489c4e058c1a41d553337
VVVladyslav <vladoszhar@gmail.com>
2023-12-30 22:50:02 +0200

    a

--- src/main/java/org/example/DatabasePopulateService.java
+++ src/main/java/org/example/DatabasePopulateService.java
@@ -1,28 +1,69 @@
 package org.example;
+import org.example.Database;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabasePopulateService {
-    public DatabasePopulateService() {
-        main(new String[]{"start"});
-    }
-    public static void main(String[] args) {
 
-        Database database = new Database();
-        StringBuilder fileContent = new StringBuilder();
+    public static void main(int[] ids, String[] names, String[] birthdates,
+                            String[] positions, int[] salaries, int[] clientNum,
+                            String[] clientName, int[] projectIds,
+                            int[] clientIds, String[] startDates, String[] endDates,
+                            int[] projectIdsForWorker, int[] workerIds) {
+        try {
+            Connection connection = Database.getInstance().getConnection();
 
-        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/populate_db.sql"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                fileContent.append(line).append("\n");
+            String sqlQuery;
+            PreparedStatement queryStatement;
+
+            sqlQuery = "INSERT INTO PUBLIC.WORKER VALUES (?, ?, ?, ?, ?)";
+            queryStatement = connection.prepareStatement(sqlQuery);
+            for (int i = 0; i < ids.length; i++) {
+                queryStatement.setInt(1, ids[i]);
+                queryStatement.setString(2, names[i]);
+                queryStatement.setDate(3, Date.valueOf(birthdates[i]));
+                queryStatement.setString(4, positions[i]);
+                queryStatement.setInt(5, salaries[i]);
+                queryStatement.addBatch();
             }
+            queryStatement.executeBatch();
 
-        } catch (IOException e) {
+            sqlQuery = "INSERT INTO PUBLIC.CLIENT VALUES (?, ?)";
+            queryStatement = connection.prepareStatement(sqlQuery);
+            for (int i = 0; i < clientNum.length; i++) {
+                queryStatement.setInt(1, clientNum[i]);
+                queryStatement.setString(2, clientName[i]);
+                queryStatement.addBatch();
+            }
+            queryStatement.executeBatch();
+
+            sqlQuery = "INSERT INTO PUBLIC.PROJECT VALUES (?, ?, ?, ?)";
+            queryStatement = connection.prepareStatement(sqlQuery);
+            for (int i = 0; i < projectIds.length; i++) {
+                queryStatement.setInt(1, projectIds[i]);
+                queryStatement.setInt(2, clientIds[i]);
+                queryStatement.setDate(3, Date.valueOf(startDates[i]));
+                queryStatement.setDate(4, Date.valueOf(endDates[i]));
+                queryStatement.addBatch();
+            }
+            queryStatement.executeBatch();
+
+            sqlQuery = "INSERT INTO PUBLIC.PROJECT_WORKER VALUES (?, ?)";
+            queryStatement = connection.prepareStatement(sqlQuery);
+            for (int i = 0; i < projectIdsForWorker.length; i++) {
+                queryStatement.setInt(1, projectIdsForWorker[i]);
+                queryStatement.setInt(2, workerIds[i]);
+                queryStatement.addBatch();
+            }
+            queryStatement.executeBatch();
+            //connection.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-
-        String contentAsString = fileContent.toString();
-        database.executeUpdate(contentAsString);
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,24 @@
 package org.example;
 
 import org.example.DatabaseQueryService.DatabaseQueryService;
+import org.example.Delet.DeletObjects;
 
 public class Main {
     public static void main(String[] args){
 
-        //new DatabaseInitService(); // SECOND
-        //new DatabasePopulateService(); // THIRD
+        DataVariables dataVariables = new DataVariables();
+
+        new DatabaseInitService(); // SECOND
+
+        DatabasePopulateService databasePopulateService = new DatabasePopulateService(); // THIRD
+        databasePopulateService.main(
+                dataVariables.ids, dataVariables.names, dataVariables.birthdates, dataVariables.positions, dataVariables.salaries,
+                dataVariables.clientNum, dataVariables.clientName,
+                dataVariables.projectIds, dataVariables.clientIds, dataVariables.startDates, dataVariables.endDates,
+                dataVariables.projectIdsForWorker, dataVariables.workerIds
+        );
+
+
         DatabaseQueryService databaseQueryService = new DatabaseQueryService();
         databaseQueryService.findMaxSalaryCountWorker();
         databaseQueryService.findMaxProjectsClient(1);
@@ -14,7 +26,7 @@ public class Main {
         databaseQueryService.findYoungestOldestPerson("OLDEST");
         databaseQueryService.findProjectPrice(32000 ,102000);
 
-        //DeletObjects deletObjects = new DeletObjects();
+        DeletObjects deletObjects = new DeletObjects();
         //deletObjects.sendDelet();
     }
 }
